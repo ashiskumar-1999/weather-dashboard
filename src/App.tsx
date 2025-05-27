6dabe3d49c6c79e4c3954f75fdf320d69f9a332e
Ashis Kumar <ashishsahoo389@gmail.com>
2025-05-27 19:51:36 +0530

    feat: Implement fetch functionality.
    Added the fetch functionality to fetch the current weather of a city and display in the weather dashboard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchBar from "./component/SearchBar/SearchBar";
+import WeatherDashboard from "./component/WeatherDashboard/WeatherDashboard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cityName, setCityName] = useState("");
+  const [weatherData, setWeatherData] = useState<any>();
 
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const result = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${
+            import.meta.env.VITE_APP_ID
+          }&units=metric`
+        );
+        const data = await result.json();
+        console.log("Result", data.name);
+        setWeatherData(data);
+      } catch (error) {
+        console.log("Error", error);
+      }
+    };
+    fetchWeather();
+  }, [cityName]);
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setCityName(e.target.value);
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar cityName={cityName} onChange={handleCityChange} />
+      {weatherData && weatherData.main && weatherData.wind && (
+        <WeatherDashboard
+          cityName={weatherData.name}
+          temprature={weatherData.main.temp}
+          feelsLikeTemprature={weatherData.main.feels_like}
+          humidity={weatherData.main.humidity}
+          windSpeed={weatherData.wind.speed}
+        />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
