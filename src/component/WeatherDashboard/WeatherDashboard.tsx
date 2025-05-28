bfb0c658fd24baf45a1ea5e4433627fafceab313
Ashis Kumar <ashishsahoo389@gmail.com>
2025-05-28 12:14:41 +0530

    fix: change the layout.

--- src/component/WeatherDashboard/WeatherDashboard.tsx
+++ src/component/WeatherDashboard/WeatherDashboard.tsx
@@ -1,5 +1,6 @@
 import styles from "./WeatherDashboard.module.css";
 import type { WeatherDataProps } from "../../types";
+import { Wind, Droplet } from "lucide-react";
 
 const WeatherDashboard = ({
   cityName,
@@ -7,20 +8,41 @@ const WeatherDashboard = ({
   feelsLikeTemprature,
   humidity,
   windSpeed,
+  weatherType,
+  weatherIcon,
 }: WeatherDataProps) => {
   return (
     <div className={styles.gridContainer}>
-      <div className={styles.currentWeather}>
-        <h2 className={styles.text}>{cityName}</h2>
+      <div className={`${styles.currentWeather} ${styles.effect}`}>
+        <p className={styles.text}>Current Weather</p>
+        <h2 className={styles.heading}>{cityName}</h2>
+        <div className={styles.temprature}>
+          <div>
+            <img
+              src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
+              alt="weather-icon"
+              className={styles.image}
+            />
+            <p className={styles.text}>{weatherType}</p>
+          </div>
+          <div>
+            <h1 className={styles.heading}>{temprature}</h1>
+            <p className={styles.text}>Feels like {feelsLikeTemprature}</p>
+          </div>
+        </div>
+        <div className={styles.otherdata}>
+          <div>
+            <Wind className={styles.text} />
+            <h3 className={styles.text}>{windSpeed} km/h</h3>
+          </div>
+          <div>
+            <Droplet className={styles.text} />
+            <h3 className={styles.text}>{humidity}%</h3>
+          </div>
+        </div>
       </div>
-      <div className={styles.map}>
-        <h1 className={styles.text}>Temperature:{temprature}</h1>
-        <p className={styles.text}>Feels like {feelsLikeTemprature}</p>
-      </div>
-      <div className={styles.forecast}>
-        <h3 className={styles.text}>Humidity: {humidity}</h3>
-        <h3 className={styles.text}>Wind Speed: {windSpeed}</h3>
-      </div>
+      <div className={`${styles.map} ${styles.effect}`}></div>
+      <div className={`${styles.forecast} ${styles.effect}`}></div>
     </div>
   );
 };
